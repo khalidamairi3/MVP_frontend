4e2975e0f480ed243d854f6d97aeed584a77187f
khalidamairi3 <khalidamairi3@gmail.com>
2020-12-16 22:24:27 -0700

    setup and add vutify

---
+++ src/views/landing.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <v-parallax
+      height=""
+      src="https://cdn.vuetifyjs.com/images/parallax/material2.jpg"
+    ></v-parallax>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "landing-page",
+};
+</script>
+
+<style lang="scss" scoped>
+</style>

402066455ae66eaada07d96dd7ce0cc7f5111aa2
khalidamairi3 <khalidamairi3@gmail.com>
2020-12-17 18:30:18 -0700

    courses-view and add course

--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import VModal from 'vue-js-modal/dist/index.nocss.js'
+import 'vue-js-modal/dist/styles.css'
 
 Vue.config.productionTip = false;
+Vue.use(VModal)
 
 new Vue({
   router,
--- src/views/courses.vue
+++ src/views/courses.vue
@@ -1,8 +1,57 @@
 <template>
     <div id="courses-view">
-
+        <v-btn @click="newCourse" id="new"> Create a new course </v-btn>
         <courseCard v-for=" course in courses " :key="course.id" :course = course />
+        <modal name="newCourse" >
 
+            <v-form class="course-form"
+    ref="form"
+    lazy-validation
+  >
+    <v-text-field
+      v-model="name"
+      label="Course Name"
+      required
+    ></v-text-field>
+
+    <v-text-field
+      v-model="department"
+      label="Department"
+      required
+    ></v-text-field>
+
+    <v-combobox
+          v-model="credits"
+          :items="[1,2,3,4,5]"
+          label="Credits"
+          outlined
+          dense
+          required
+        ></v-combobox>
+
+
+
+    <v-btn
+      :disabled="disabled"
+      color="primary"
+      class="mr-4"
+      @click="addCourse"
+    >
+      Add Course
+    </v-btn>
+     
+    <v-alert v-if="courseErr"
+      dense
+      type="error"
+    >
+     Something went wrong 
+    </v-alert>
+    
+  </v-form>
+    
+
+
+        </modal>
     </div>
 </template>
 
@@ -22,6 +71,11 @@ import courseCard from "../components/course"
             return {
                 courses: [],
                 err:false,
+                disabled:false,
+                name:"",
+                department:"",
+                credits:0,
+                courseErr : false
             }
         },
         methods: {
@@ -44,8 +98,40 @@ import courseCard from "../components/course"
           })
                 
                 
-            }
+            },
+             newCourse(){
+            this.$modal.show('newCourse')
+
         },
+        
+        addCourse(){
+            if(this.credits != 0 && this.name != "" && this.deppartment != ""){
+                this.disabled=true;
+                this.courseErr = false;
+            axios.request({
+                url:"http://127.0.0.1:5000/api/courses",
+                method:"POST",
+                data:{
+                    loginToken:cookies.get("token"),
+                    courseName : this.name,
+                    credits:this.credits,
+                    department:this.department
+                },
+                headers:{
+                    "Content-Type": "application/json",
+                }
+            }).then((response)=>{
+                this.disabled=false;
+                this.$modal.hide('newCourse');
+                this.courses.push(response.data);
+
+            }).catch(()=>{
+                this.disabled=false;
+                this.courseErr = true;
+            })}
+        },
+        }
+
         
         
     }
@@ -62,5 +148,14 @@ import courseCard from "../components/course"
     align-items: center;
     justify-items: center;
 }
+#new{
+    grid-column: 1/-1;
+}
 
+.course-form{
+    
+    width: 80%;
+    padding: 5%;
+    margin-left: 7.5% ;
+}
 </style>
